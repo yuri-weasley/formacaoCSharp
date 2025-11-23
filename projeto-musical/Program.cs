9e3b054d8d425880b86e7f63413fc3087dd2bb96
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2025-11-23 14:33:48 -0400

    Continuação do curso

--- projeto-musical/Program.cs
+++ projeto-musical/Program.cs
@@ -1,3 +1,43 @@
 ﻿// Screen Sound
 string mensagemDeBoasVindas = "Boas Vindas ao Screen Sound!";
-Console.WriteLine(mensagemDeBoasVindas);
+
+void ExibirMensagemDeBoasVindas()
+{
+    Console.WriteLine(@"
+    
+╭━━━╮╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭━━━╮╱╱╱╱╱╱╱╱╱╱╭╮
+┃╭━╮┃╱╱╱╱╱╱╱╱╱╱╱╱╱╱┃╭━╮┃╱╱╱╱╱╱╱╱╱╱┃┃
+┃╰━━┳━━┳━┳━━┳━━┳━╮╱┃╰━━┳━━┳╮╭┳━╮╭━╯┃
+╰━━╮┃╭━┫╭┫┃━┫┃━┫╭╮╮╰━━╮┃╭╮┃┃┃┃╭╮┫╭╮┃
+┃╰━╯┃╰━┫┃┃┃━┫┃━┫┃┃┃┃╰━╯┃╰╯┃╰╯┃┃┃┃╰╯┃
+╰━━━┻━━┻╯╰━━┻━━┻╯╰╯╰━━━┻━━┻━━┻╯╰┻━━╯
+    ");
+    Console.WriteLine(mensagemDeBoasVindas);
+}
+
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para mostrar a média de avaliações de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+    switch (opcaoEscolhidaNumerica)
+    {
+        case 1: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);   break;
+        case 2: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica); break;
+        case 3: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica); break;
+        case 4: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica); break;
+        case -1: Console.WriteLine("Até logo! :)"); 
+        break;
+        default: Console.WriteLine("Opção inválida, tente novamente."); 
+        break;
+    }
+}
+
+ExibirMensagemDeBoasVindas();
+ExibirOpcoesDoMenu();
